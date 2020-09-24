b909975fafce23a83f4febbdf702a3809f161748
b3b3r <b3b3rseg@gmail.com>
2020-09-24 09:53:05 +0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["babel-polyfill", "./src/snake/script.js"],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    open: true,
+    contentBase: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // tous les fichiers js
+        exclude: /node_modules/, // excluts node_modules
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
+        }
+      }
+    ]
+  }
+};
